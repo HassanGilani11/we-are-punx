2a41bfb1bfe4b14ce876bb8f92ba10fafd466f8c
Hassangilani11 <hassangillani12@gmail.com>
2024-12-18 21:17:08 +1100

    first commit

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"cE8<Vpgz0.HHA@vR`QxT.}6>l]U~pS-9^Yq=v}_uFV6DF,H5zqxX-br`B+]Y< cE";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:888;}
